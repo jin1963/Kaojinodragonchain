dad4dc88a9b4b195356c8ad4e11a8d00fbcb2195
jin1963 <kaojino89@gmail.com>
2025-07-17 05:30:40 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+  abi: [
+    {
+      constant: true,
+      inputs: [{ name: "_owner", type: "address" }],
+      name: "balanceOf",
+      outputs: [{ name: "balance", type: "uint256" }],
+      type: "function",
+    }
+  ],
+  kjc: {
+    address: "0xd479ae350dc24168e8db863c5413c35fb2044ecd"
+  },
+  g3x24: {
+    address: "0x6cfD8Fe423F20F94825b5edB1E94068fBea19dC9"
+  },
+  lydia: {
+    address: "0x0fa662697d93Eb024f411E416f681EA7FECFcF96"
+  }
+};
